daf33b5cddce69c05ce97e3aac223df6f6d72bba
Paweł Samsel <pawel.samsel@absolvent.pl>
2019-03-29 10:32:27 +0100

    Adding missing public

---
+++ public/precache-manifest.c05dfb5c576519db26a2e1aaaa4a1a17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7d90010c5ff7e5d8b851",
+    "url": "/static/js/main.7d90010c.chunk.js"
+  },
+  {
+    "revision": "b92776b32dada62a7eaa",
+    "url": "/static/js/2.b92776b3.chunk.js"
+  },
+  {
+    "revision": "7d90010c5ff7e5d8b851",
+    "url": "/static/css/main.bce3e077.chunk.css"
+  },
+  {
+    "revision": "427b039ea2045bc0cfe5144b38f1dc6a",
+    "url": "/index.html"
+  }
+];
